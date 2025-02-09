3111faab2fd8548abfa2794fc0df165d9f319138
adamoah <100427408+adamoah@users.noreply.github.com>
2025-02-08 19:04:34 -0500

    Update for_fun.py

--- for_fun.py
+++ for_fun.py
@@ -14,28 +14,28 @@ processor = Wav2Vec2Processor.from_pretrained(model_n)
 model = Wav2Vec2ForCTC.from_pretrained(model_n)
 
 # define movement keywords
-LEFT, RIGHT, UP, DOWN, STOP, QUIT = 'MOVE LEFT', 'MOVE RIGHT', 'MOVE UP', 'MOVE DOWN', 'STOP', 'QUIT'
+LEFT, RIGHT, UP, DOWN, STOP, QUIT = 'LEFT', 'RIGHT', 'UP', 'DOWN', 'STOP', 'QUIT'
 
 # audio and input control objects
 r = sr.Recognizer()
 quit = False
 controller = Controller()
-selected = None
+selected = []
 
 # function to return keyboard input based on keyword detected
 def select_key(output, prev):
 
     if LEFT in output:
-        print('You Said: MOVE LEFT')
+        print('You Said: LEFT')
         return Key.left
     elif RIGHT in output:
-        print('You Said: MOVE RIGHT')
+        print('You Said: RIGHT')
         return Key.right
     elif UP in output:
-        print('You Said: MOVE UP')
+        print('You Said: UP')
         return Key.up
     elif DOWN in output:
-        print('You Said: MOVE DOWN')
+        print('You Said: DOWN')
         return Key.down
     elif STOP in output: # input to stop pressing current key
         print('You Said: STOP')
@@ -62,15 +62,23 @@ with sr.Microphone(sample_rate=16000) as source:
         text = processor.batch_decode(tokens)[0]
         print(text)
         
+        previous = selected[-2] if len(selected) > 1 else None
         # determine key to select
-        selected = select_key(text, selected)
+        selected.append(select_key(text, previous))
         
         # initiate keybaord press or release
-        if selected == Key.esc:
+        if selected[-1] == Key.esc:
             quit = True
-        elif selected == Key.space:
-            controller.release(selected)
-        elif selected is not None:
-            controller.press(selected)
+        elif selected[-1] == Key.space:
+
+            for input in selected: # release all previously store inputs
+                if input is not None:
+                    controller.release(input)
+            selected = [] # set array to empty
+
+        elif selected[-1] is not previous: # initiate press only if new key has been selected
+            controller.press(selected[-1])
+        
+        print(selected)
     
     print('Exiting...')
